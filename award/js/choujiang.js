8a9557bb532ab5a4f700c6abdf8b596654de7932
mr-zx <1457120101@qq.com>
2016-09-18 15:22:57 +0800

    add award

---
+++ award/js/choujiang.js
@@ -0,0 +1,84 @@
+﻿var lottery = {
+    index: -1,	//当前转动到哪个位置，起点位置
+    count: 0,	//总共有多少个位置
+    timer: 0,	//setTimeout的ID，用clearTimeout清除
+    speed: 20,	//初始转动速度
+    times: 0,	//转动次数
+    cycle: 50,	//转动基本次数：即至少需要转动多少次再进入抽奖环节
+    prize: -1,	//中奖位置
+    init: function (id) {
+        if ($("#" + id).find(".lottery-unit").length > 0) {
+            $lottery = $("#" + id);
+            $units = $lottery.find(".lottery-unit");
+            this.obj = $lottery;
+            this.count = $units.length;
+            $lottery.find(".lottery-unit-" + this.index + " a").addClass("active");
+        };
+    },
+    roll: function () {
+        var index = this.index;
+        var count = this.count;
+        var lottery = this.obj;
+        $(lottery).find(".lottery-unit-" + index + " a").removeClass("active");
+        index += 1;
+        if (index > count - 1) {
+            index = 0;
+        };
+        $(lottery).find(".lottery-unit-" + index + " a").addClass("active");
+        this.index = index;
+        return false;
+    },
+    stop: function (index) {
+        this.prize = index;
+        return false;
+    }
+};
+function roll() {
+    lottery.times += 1;
+    lottery.roll();
+    if (lottery.times > lottery.cycle + 10 && lottery.prize == lottery.index) {
+        clearTimeout(lottery.timer);
+        lottery.prize = -1;
+        lottery.times = 0;
+        isRunning = false;
+        //-----------==========================此处改为获奖弹窗========================================================
+        alert('我中奖啦');
+    } else {
+        if (lottery.times < lottery.cycle) {
+            lottery.speed -= 10;
+        } else if (lottery.times == lottery.cycle) {
+            //-----------==========================此处从后台获得中奖的index [0,11]========================================================
+            var index = Math.random() * (lottery.count) | 0;
+            //-----------===================================================================================================================
+            lottery.prize = index;
+        } else {
+            if (lottery.times > lottery.cycle + 10 && ((lottery.prize == 0 && lottery.index == 7) || lottery.prize == lottery.index + 1)) {
+                lottery.speed += 110;
+            } else {
+                lottery.speed += 20;
+            }
+        }
+        if (lottery.speed < 40) {
+            lottery.speed = 40;
+        };
+        //console.log(lottery.times+'^^^^^^'+lottery.speed+'^^^^^^^'+lottery.prize);
+        lottery.timer = setTimeout(roll, lottery.speed);
+    }
+    return false;
+}
+//开始抽奖
+function startCJ() {
+    lottery.init('lottery');
+    $("#lottery a").click(function () {
+        if (!isRunning) {
+            lottery.speed = 100;
+            roll();
+            isRunning = true;
+            return false;
+        }
+    });
+}
+var isRunning = false;
+$(function () {
+    startCJ();
+});
