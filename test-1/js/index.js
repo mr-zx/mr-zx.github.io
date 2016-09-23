0706c3a10356f7e16f5b5f9b6e9e4260cedc82ca
mr-zx <1457120101@qq.com>
2016-09-23 12:09:06 +0800

    add test-1

---
+++ test-1/js/index.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    initBanner();
+});
+function initBanner() {
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: 3000,
+        pagination: '.pagination',
+        paginationClickable: true
+    });
+}
